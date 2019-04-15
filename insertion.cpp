2d8d38b3570558cd94eb54c94056d6d868c514d1
vishwajeet-agrawal <vishwa.grawal@gmail.com>
2019-04-15 21:31:30 +0530

    insertion fornt

--- insertion.cpp
+++ insertion.cpp
@@ -1,3 +1,34 @@
 #include "functions.h"
-#include <iostream>
-
+#include <fstream>
+#include <algorithm>
+#define MAX_ARRAY_SIZE 100000
+void InsertArray(int* input, int size, FileHandler& fh);
+using namespace std;
+int main(int argc, char* argv[]){
+    if (argc<3){
+        return 1;
+    }
+    FileManager fm;
+    FileHandler fh = fm.OpenFile(argv[1]);
+    int counter=0;
+    fstream fs;
+    fs.open(argv[2]);
+    int* input = new int[MAX_ARRAY_SIZE];
+    while(true){
+        while(counter<MAX_ARRAY_SIZE && !fs.eof()){
+            fs>>*(input+counter);
+            counter++;
+        }
+        InsertArray(input,counter,fh);
+        if(counter==MAX_ARRAY_SIZE){
+            counter=0;
+        }
+        else{
+            break;
+        }
+    }
+}
+void InsertArray(int* input, int size, FileHandler& fh){
+    sort(input,input+size);
+    
+}
