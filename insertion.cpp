516d1983da563adb40718f6a7511f93332217682
vishwajeet-agrawal <vishwa.grawal@gmail.com>
2019-04-15 22:47:28 +0530

    insertion_not_changed

--- insertion.cpp
+++ insertion.cpp
@@ -6,7 +6,8 @@
 void InsertArray(int* input, int size, FileHandler& fh);
 pair<int,bool> ShiftPage (PageHandler &ph, int index, int value);
 void Insertion(FileHandler& fh, int t);
-
+int Insertion(FileHandler& fh, int t, int fv, int fpgn);
+pair<int,int> boundMegaBinarySearch(FileHandler& fh, int t,char LU,int first_pgn);
 using namespace std;
 int main(int argc, char* argv[]){
     if (argc<3){
@@ -54,6 +55,40 @@ pair<int,bool> ShiftPage (PageHandler &ph, int index, int value) { // indexing s
 	if(flag==true) return (make_pair(t,false));
 	else return make_pair(t,true);
 } 
+int Insertion(FileHandler& fh, int t, int fv, int fpgn){
+    pair<int,int> mbsr = boundMegaBinarySearch(fh,t,'U',fpgn);
+    int pg = mbsr.first;
+	int pos = mbsr.second;
+	PageHandler ph = fh.LastPage();
+
+	int last_pgn = ph.GetPageNum();
+	fh.UnpinPage(last_pgn);
+	fh.FlushPages();
+		PageHandler ph1 = fh.NextPage(pg-1);
+		pair<int,bool> val = ShiftPage(ph1,pos-1,t);
+		fh.MarkDirty(pg++);
+		// fh.UnpinPage(pg++);
+		// fh.UnpinPage(pg++);
+		// fh.FlushPage(0);
+		
+		// ph1 = fh.FirstPage();
+
+		while(true){
+			if (pg>last_pgn){
+				break;
+			}
+			ph1 = fh.PageAt(pg);
+			val = ShiftPage(ph1,0,val.first);
+			fh.MarkDirty(pg++);
+			// fh.FlushPage(pg-1);
+		}
+		if (val.second==true){
+			ph1 = fh.NewPage();
+			ShiftPage(ph1,0,val.first);
+			fh.MarkDirty(ph1.GetPageNum());
+		}
+		fh.FlushPages();
+}
 void Insertion(FileHandler& fh, int t){
 	pair<int,int> mbsr = boundMegaBinarySearch(fh,t,'U');
 	int pg = mbsr.first;
@@ -375,5 +410,159 @@ BSResult SearchLastPage (PageHandler& ph, int t,char type) {
 			}
 		}		
 		return BSResult('F',pg,PAGE_CONTENT_SIZE/4-1);
+	}
+}
+
+pair<int,int> boundMegaBinarySearch(FileHandler& fh, int t,char LU,int first_pgn){
+	PageHandler pgi = fh.PageAt(first_pgn);
+	PageHandler pgf = fh.LastPage();
+
+	int last_pgn = pgf.GetPageNum();
+
+	std::cout<<"The file has "<<last_pgn-first_pgn+1<<" pages"<<endl;
+	//checking the first page
+
+	//check if first page = last page
+	if (first_pgn==last_pgn){
+		fh.UnpinPage(last_pgn);
+		BSResult bsr = SearchLastPage(pgi,t,LU);
+		if (bsr.type=='L'){
+			return make_pair(last_pgn,0);
+		}
+		else if(bsr.type=='R'){
+			return make_pair(last_pgn+1,0);
+		}
+		else {
+			return bsr.result;
+		}
+	}
+
+	
+	BSResult bsr = BoundBinarySearch(pgi,t,LU);
+	fh.UnpinPage(pgi.GetPageNum());
+	BSResult& bsref = bsr;
+	if (bsref.type=='F'){
+		return bsr.result;
+	}
+	else if(bsref.type=='L'){
+		return make_pair(first_pgn,0);
+	}
+	else if(bsref.type=='R'){
+		//checking the last page
+		BSResult bsr1 = SearchLastPage(pgf,t,LU);
+		fh.UnpinPage(pgf.GetPageNum());
+		bsref = bsr1;
+		if (bsref.type=='F'){
+			return bsref.result;
+		}
+		else if (bsref.type=='R'){
+			return make_pair(last_pgn+1,0);
+		}
+		else if (bsref.type=='L'){
+			//proceed
+		}
+	}
+	// somewhere in the middle
+	int low_pg = first_pgn+1;
+	int hi_pg = last_pgn -1;
+	int mid_pg;
+	int page_no;
+	int no_pages;
+	PageHandler *pha = new PageHandler[BUFFER_SIZE];
+	while(true){
+		if (hi_pg<low_pg){
+			if (bsref.type=='L')
+				return make_pair(bsref.result.first,0);
+			else if (bsref.type=='R')
+				return make_pair(bsref.result.first+1,0);
+		}
+		else{
+			no_pages=0;
+			mid_pg = (hi_pg+low_pg)/2;
+			page_no = mid_pg;
+			try{
+				//loading pages into buffer
+				while(true){
+					if (no_pages==BUFFER_SIZE || no_pages>hi_pg){
+						throw exception();
+					}
+					pha[no_pages]=(fh.PageAt(page_no));
+					page_no++;
+					no_pages++;	
+				}
+			}
+			catch(...){
+				cout<<"No more space in buffer, initiating binary search in pinned pages"<<endl;
+				cout<<no_pages<<" pages loaded in buffer"<<endl;
+				int hi = no_pages-1;
+				int lo = 0;
+				int mid;
+				pair<int,int> ans;
+				//check in first page 
+				BSResult bsr1 = BoundBinarySearch(pha[0],t,LU);
+				bsref = bsr1;
+				if (bsref.type=='F'){
+					ans = bsref.result;
+				}
+				else if(bsref.type=='L'){
+					hi_pg=mid_pg-1;
+					for(int i=0;i<no_pages;i++){
+						fh.UnpinPage(pha[i].GetPageNum());
+					}
+					continue;
+				}
+				else if(bsref.type=='R'){
+					if (hi==lo){
+						ans= make_pair(bsref.result.first+1,0);
+					}
+					//cheking in last page in buffer
+					BSResult bsr1 = BoundBinarySearch(pha[hi],t,LU);
+					bsref = bsr1;
+					if (bsref.type=='F'){
+						ans = bsref.result;
+					}
+					else if (bsref.type=='R'){
+						low_pg = mid_pg+hi+1;
+						for(int i=0;i<no_pages;i++){
+							fh.UnpinPage(pha[i].GetPageNum());
+						}
+						continue;
+					}
+					else{ //bsref.type = 'L'
+						lo++;
+						hi--;
+						while(true){
+							if (hi<lo) break;
+							mid = (hi+lo)/2;
+							BSResult bsr1 = BoundBinarySearch(pha[mid],t,LU);
+							bsref = bsr1;
+							if (bsref.type=='L'){
+								hi = mid-1;
+							}
+							else if(bsref.type=='R'){
+								lo = mid+1;
+							}
+							else{
+								break;
+							}
+						}
+						if (bsref.type=='L'){
+							ans = make_pair(bsref.result.first,0);
+						}
+						else if (bsref.type=='R'){
+							ans = make_pair(bsref.result.first+1,0);
+						}
+						else{//type='F'
+							ans = bsref.result;
+						}
+					}
+					
+				}
+				for(int i=0;i<no_pages;i++){
+					fh.UnpinPage(pha[i].GetPageNum());
+				}
+				return ans;
+			}
+		}
 	}
 }
